86b6350df3a85d1f0271c6c5c02fec66c91a74e5
Minh Ngo <bbbbbb267@gmail.com>
2023-11-23 10:00:28 +0100

    update API Endpoint

--- staging_bus_delay/views.py
+++ staging_bus_delay/views.py
@@ -2,50 +2,16 @@ from django.http import HttpResponse, JsonResponse, FileResponse
 from rest_framework.decorators import api_view
 import time
 from staging_bus_delay.pipeline.extract import (
-    extract_data_weekday_weekend_hour,
-    extract_data_weekday_weekend_hour_location_incident,
     extract_full_data
 )
 from staging_bus_delay.pipeline.transform import (
-    transform_data_weekday_weekend_hour,
-    transform_data_weekday_weekend_hour_location_incident,
     transform_full_data
 )
 from staging_bus_delay.pipeline.load import (
-    load_data_weekday_weekend_hour,
-    load_data_weekday_weekend_hour_location_incident,
     load_full_data
 )
 from django.core.cache import cache
 import json
-
-
-@api_view(['GET'])
-def stagingWeekdayWeekendHourBusDelayDataPipeline(request):
-    # Start pipeline
-    start_time = time.time()
-    # Extraction
-    data = extract_data_weekday_weekend_hour()
-    # Transformation
-    transformed_data = transform_data_weekday_weekend_hour(data)
-    # Load
-    load_data_weekday_weekend_hour(transformed_data)
-    processing_time = time.time() - start_time
-    return JsonResponse({"processing_time": processing_time})
-
-
-@api_view(['GET'])
-def stagingWeekdayWeekendHourLocationIncidentBusDelayDataPipeline(request):
-    # Start pipeline
-    start_time = time.time()
-    # Extraction
-    data = extract_data_weekday_weekend_hour_location_incident()
-    # Transformation
-    transformed_data = transform_data_weekday_weekend_hour_location_incident(data)
-    # Load
-    load_data_weekday_weekend_hour_location_incident(transformed_data)
-    processing_time = time.time() - start_time
-    return JsonResponse({"processing_time": processing_time})
 
 
 @api_view(['GET'])
--- staging_weather/views.py
+++ staging_weather/views.py
@@ -2,54 +2,17 @@ from django.http import HttpResponse, JsonResponse, FileResponse
 from rest_framework.decorators import api_view
 import time
 from staging_weather.pipeline.extract import (
-    extract_data_year_month,
-    extract_data_year_month_day,
-    extract_full_data,
-    extract_data_weekday_weekend_hour
+    extract_full_data
 )
 from staging_weather.pipeline.transform import (
-    transform_data_year_month,
-    transform_data_year_month_day,
-    transform_full_data,
-    transform_data_weekday_weekend_hour
+    transform_full_data
 )
 from staging_weather.pipeline.load import (
-    load_data_year_month,
-    load_data_year_month_day,
-    load_full_data,
-    load_data_weekday_weekend_hour
+    load_full_data
 )
 from django.core.cache import cache
 from pipeline_api.settings import CACHE_TTL
 import json
-
-
-@api_view(['GET'])
-def stagingYearMonthWeatherDataPipeline(request):
-    # Start pipeline
-    start_time = time.time()
-    # Extraction
-    data = extract_data_year_month()
-    # Transformation
-    transformed_data = transform_data_year_month(data)
-    # Load
-    load_data_year_month(transformed_data)
-    processing_time = time.time() - start_time
-    return JsonResponse({"processing_time": processing_time})
-
-
-@api_view(['GET'])
-def stagingYearMonthDayWeatherDataPipeline(request):
-    # Start pipeline
-    start_time = time.time()
-    # Extraction
-    data = extract_data_year_month_day()
-    # Transformation
-    transformed_data = transform_data_year_month_day(data)
-    # Load
-    load_data_year_month_day(transformed_data)
-    processing_time = time.time() - start_time
-    return JsonResponse({"processing_time": processing_time})
 
 
 @api_view(['GET'])
@@ -72,18 +35,4 @@ def stagingFullWeatherDataPipeline(request):
     # Cache data
     data_to_cache = json.dumps({"processing_time": processing_time})
     cache.set(cached_key, data_to_cache, timeout=CACHE_TTL)
-    return JsonResponse({"processing_time": processing_time})
-
-
-@api_view(['GET'])
-def stagingWeekdayWeekendHourWeatherDataPipeline(request):
-    # Start pipeline
-    start_time = time.time()
-    # Extraction
-    data = extract_data_weekday_weekend_hour()
-    # Transformation
-    transformed_data = transform_data_weekday_weekend_hour(data)
-    # Load
-    load_data_weekday_weekend_hour(transformed_data)
-    processing_time = time.time() - start_time
     return JsonResponse({"processing_time": processing_time})
